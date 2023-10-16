ae1f686fc535320ef44221b7dcdc1f2b4efe31ec
shtl00 <parmarsheetal@ymail.com>
2023-10-16 12:05:49 +0100

    first commit

---
+++ src/test/java/org/example/OpenUrl.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenUrl {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/");
+
+
+
+    }
+
+}
